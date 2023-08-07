1831c595fe9657da6836a81492b3f48c2b3ae80b
Ligia88 <129026864+Ligia88@users.noreply.github.com>
2023-08-07 09:20:46 +0000

    commit

--- src/models.py
+++ src/models.py
@@ -7,16 +7,6 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Characters(Base):
-    __tablename__ = 'characters'
-    id = Column(Integer, primary_key=True)
-    Name = Column(String(20))
-    Height = Column(Integer)
-    Mass = Column(Integer)
-    HairColor = Column(String(20))
-    PlanetID = Column(Integer, ForeignKey('planets.ID'))
-    Planet = relationship("Planets")
-
 class Planets(Base):
     __tablename__ = 'planets'
     ID = Column(Integer, primary_key=True)
@@ -25,29 +15,66 @@ class Planets(Base):
     Rotation = Column(Integer)
     Terrain = Column(String(20))
 
+class Characters(Base):
+    __tablename__ = 'characters'
+    ID = Column(Integer, primary_key=True)
+    Name = Column(String(30))
+    Height = Column(Integer)
+    Mass = Column(Integer)
+    HairColor = Column(String(30))
+    EyeColor = Column(String(30))
+    SkinColor = Column(String(30))
+    BirthYear = Column(String(30))
+    Gender = Column(String(30))
+    Planet = Column(Integer, ForeignKey('planets.ID'))
+    planet = relationship(Planets)
+
 class Starships(Base):
     __tablename__ = 'starships'
     ID = Column(Integer, primary_key=True)
-    Name = Column(String(20))
+    Name = Column(String(30))
     Model = Column(String(50))
     Length = Column(Integer)
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    ID = Column(Integer, primary_key=True)
+    TypeID = Column(Integer, ForeignKey('types.ID'))
+    FavoriteID = Column(Integer)
 
 class FavoritePlanets(Base):
     __tablename__ = 'favorite_planets'
     ID = Column(Integer, primary_key=True)
-    PlanetID = Column(Integer, ForeignKey('planets.ID'), unique=True)
+    FavoriteID = Column(Integer, ForeignKey('favorites.ID'))
+    PlanetID = Column(Integer, ForeignKey('planets.ID'))
 
 class FavoriteCharacters(Base):
     __tablename__ = 'favorite_characters'
     ID = Column(Integer, primary_key=True)
-    CharacterID = Column(Integer, ForeignKey('characters.id'), unique=True)
+    FavoriteID = Column(Integer, ForeignKey('favorites.ID'))
+    CharacterID = Column(Integer, ForeignKey('characters.ID'))
 
 class FavoriteStarships(Base):
     __tablename__ = 'favorite_starships'
     ID = Column(Integer, primary_key=True)
-    StarshipID = Column(Integer, ForeignKey('starships.ID'), unique=True)
+    FavoriteID = Column(Integer, ForeignKey('favorites.ID'))
+    StarshipID = Column(Integer, ForeignKey('starships.ID'))
 
-    def to_dict(self):
+class Types(Base):
+    __tablename__ = 'types'
+    ID = Column(Integer, primary_key=True)
+    Type = Column(String(20))
+
+class Users(Base):
+    __tablename__ = 'users'
+    ID = Column(Integer, primary_key=True)
+    Username = Column(String(20), unique=True)
+    Password = Column(String(20), unique=True)
+    Email = Column(String(30), unique=True)
+    FavoritesID = Column(Integer, ForeignKey('favorites.ID'), unique=True)
+
+
+def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
